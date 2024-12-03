d00197faf1defbd6ebe3477b211974e370b434bd
zolismo <140122910+zolismo@users.noreply.github.com>
2024-12-03 15:13:20 -0300

    .
    .

--- src/main/java/br/com/opusnet/projetoapiidoscrias/controlls/screencontrol/HomeSceneControl.java
+++ src/main/java/br/com/opusnet/projetoapiidoscrias/controlls/screencontrol/HomeSceneControl.java
@@ -6,8 +6,12 @@ import javafx.scene.control.Button;
 public class HomeSceneControl {
     @FXML
     public static Button b_newgame;
-
     public HomeSceneControl(){
+
     }
 
+
+    public void setAction(){
+
+    }
 }
--- src/main/java/br/com/opusnet/projetoapiidoscrias/scene/HomeScene.java
+++ src/main/java/br/com/opusnet/projetoapiidoscrias/scene/HomeScene.java
@@ -1,5 +1,6 @@
 package br.com.opusnet.projetoapiidoscrias.scene;
 
+import br.com.opusnet.projetoapiidoscrias.controlls.GameLoop;
 import br.com.opusnet.projetoapiidoscrias.controlls.screencontrol.HomeSceneControl;
 import br.com.opusnet.projetoapiidoscrias.util.SizeScreen;
 import br.com.opusnet.projetoapiidoscrias.util.Updatable;
@@ -15,19 +16,24 @@ import java.io.IOException;
 public class HomeScene extends Scene implements Updatable {
 
     private HomeSceneControl homeSceneControl;
+    private Button buttonNewGame;
+    private GameLoop gameLoop;
 
     public HomeScene(Parent root, Stage stage){
         super(root);
-        homeSceneControl = new HomeSceneControl();
+        buttonNewGame = new Button();
+
         stage.setScene(this);
         stage.setTitle("Valus");
         stage.show();
-
+        gameLoop = new GameLoop(this);  // Passa a si mesma como o Updatable
+        new Thread(gameLoop).start();  // Inicia o game loop em uma thread separada
     }
 
     @Override
     public void update() {
-
+        //System.out.println("SADasdfasfdas");
+        //HomeSceneControl.b_newgame = buttonNewGame;
     }
 
     @Override
