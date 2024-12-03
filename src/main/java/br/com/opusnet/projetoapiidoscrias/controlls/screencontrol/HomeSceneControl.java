bbedf7a5fb665dc3731ed6d3cb60d4d6e6c7d7ad
Davestrekher <davigabrielli20005@gmail.com>
2024-12-03 18:39:29 -0300

    botão
    botão corrigido(oh yeah(

--- src/main/java/br/com/opusnet/projetoapiidoscrias/controlls/screencontrol/HomeSceneControl.java
+++ src/main/java/br/com/opusnet/projetoapiidoscrias/controlls/screencontrol/HomeSceneControl.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Button;
 public class HomeSceneControl {
 
     @FXML
-    public Button b_newgame;  // O botão será injetado aqui
+    public Button b_newgame = new Button();  // O botão será injetado aqui
 
 
     @FXML
