eb693a360e9d0aa4a96a8b155a0d1d808de59e96
zolismo <140122910+zolismo@users.noreply.github.com>
2024-12-07 21:40:34 -0300

    .
    tá tendo bugs

--- src/main/java/br/com/opusnet/projetoapiidoscrias/scene/SceneOne.java
+++ src/main/java/br/com/opusnet/projetoapiidoscrias/scene/SceneOne.java
@@ -15,21 +15,19 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.util.List;
+public class SceneOne extends Scene implements Updatable, ScreemInterface {
 
-public class SceneOne extends Scene  implements Updatable, ScreemInterface {
+    private final GameLoop gameLoop;
+    private final SceneOneController controller;
+    private final Stage stage;
+    private double valueEnime = 250;
+    private final String equacionEnime = "x² - 500x + 62500 = 0";
+    private double respostPerson;
 
-    private GameLoop gameLoop;
-    private SceneOneController controller;
-    private Stage stage;
-    double valueEnime = 250;
-    String equacionEnime = "xˆ2 -500x + 62500 = 0";
-    double respostPerson = 0;
+    private double[] valueSelected = {0, 0};
+    private int buttonPressed = 0;
 
-    double[] valueSelected = {0,0};
-    int buttonPressed = 0;
-
-   // boolean[] buttonPressed = {false,false,false,false};
-
+    private boolean sentinel = true;
 
     public SceneOne(Parent root, Stage stage, SceneOneController controller) {
         super(root);
@@ -41,186 +39,84 @@ public class SceneOne extends Scene  implements Updatable, ScreemInterface {
         new Thread(gameLoop).start();
     }
 
-    public void setValuesPerson(){
-        controller.b_char1.setText(" 100");
+    public void setValuesPerson() {
+        controller.b_char1.setText("200");
         controller.b_char2.setText("90");
-        controller.b_char3.setText(" 50");
+        controller.b_char3.setText("50");
         controller.b_char4.setText("20");
     }
 
     @Override
     public void update() {
+        System.out.println("Value 1: " + valueSelected[0]);
+        System.out.println("Value 2: " + valueSelected[1]);
+        System.out.println("Button Pressed: " + buttonPressed);
+        System.out.println("Resposta calculada: " + respostPerson);
+        checkAnswer();
 
-
-        Platform.runLater(()->{
-            if(LifeGame.lifeGame == 0){
-                controller.t_level1.setVisible(true);
-                controller.t_level1.setText("Você perdeu");
-                gameLoop.stop();
-            }
-
-            checkAnswer();
-        });
     }
 
     @Override
     public void render() {
-        Platform.runLater(()->{
+        Platform.runLater(() -> {
+            if (LifeGame.lifeGame == 0) {
+                controller.t_level1.setVisible(true);
+                controller.t_level1.setText("Você perdeu");
+                gameLoop.stop();
+            }
             moveEnime();
-            buttonPressed();
-            personSelect();
+            handleButtonPress();
+            handlePersonSelect();
         });
     }
 
-
-
-    private void buttonPressed(){
-        if(controller.b_sub.isPressed()){
-            if(buttonPressed == 0){
-                buttonPressed = 1;
-            }
-        }
-        if(controller.b_add.isPressed()){
-            if(buttonPressed == 0){
-                buttonPressed = 2;
-            }
-        }
-        if(controller.b_mult.isPressed()){
-            if(buttonPressed == 0){
-                buttonPressed = 3;
-            }
-        }
-        if(controller.b_div.isPressed()){
-            if(buttonPressed == 0){
-                buttonPressed = 4;
-            }
-        }
+    private void handleButtonPress() {
+        if (controller.b_sub.isPressed() && buttonPressed == 0) buttonPressed = 1;
+        else if (controller.b_add.isPressed() && buttonPressed == 0) buttonPressed = 2;
+        else if (controller.b_mult.isPressed() && buttonPressed == 0) buttonPressed = 3;
+        else if (controller.b_div.isPressed() && buttonPressed == 0) buttonPressed = 4;
     }
 
-    private void personSelect(){
-        if(controller.b_char1.isPressed()){
-
-            if(valueSelected[0] == 0){
-                valueSelected[0] = Double.parseDouble(controller.b_char1.getText());
-            }else if(valueSelected[1]== 0){
-                valueSelected[1] = Double.parseDouble(controller.b_char1.getText());
-            }
-
-/*
-            if(!buttonPressed[1]){
-                buttonPressed[1] = true;
-            }else{
-                buttonPressed[1] = false;
-            }*/
-        }
-        if(controller.b_char2.isPressed()){
-
-            if(valueSelected[0] == 0){
-                valueSelected[0] = Double.parseDouble(controller.b_char2.getText());
-            }else if(valueSelected[1]== 0){
-                valueSelected[1] = Double.parseDouble(controller.b_char2.getText());
-            }
-
-/*
-            if(!buttonPressed[2]){
-                buttonPressed[2] = true;
-            }else{
-                buttonPressed[2] = false;
-            }
-  */
-        }
-
-        if(controller.b_char3.isPressed()){
-
-
-            if(valueSelected[0] == 0){
-                valueSelected[0] = Double.parseDouble(controller.b_char3.getText());
-            }else if(valueSelected[1]== 0){
-                valueSelected[1] = Double.parseDouble(controller.b_char3.getText());
-            }
-            /*
-            if(!buttonPressed[3]){
-                buttonPressed[3] = true;
-            }else{
-                buttonPressed[3] = false;
-            }
-
-             */
-        }
-        if(controller.b_char4.isPressed()){
-
-
-            if(valueSelected[0] == 0){
-                valueSelected[0] = Double.parseDouble(controller.b_char4.getText());
-            }else if(valueSelected[1]== 0){
-                valueSelected[1] = Double.parseDouble(controller.b_char4.getText());
-            }
-
-            /*
-            if(!buttonPressed[4]){
-                buttonPressed[4] = true;
-            }else{
-                buttonPressed[4] = false;
-            }
-
-             */
-        }
+    private void handlePersonSelect() {
+        if (controller.b_char1.isPressed()) setSelection(Double.parseDouble(controller.b_char1.getText()));
+        else if (controller.b_char2.isPressed()) setSelection(Double.parseDouble(controller.b_char2.getText()));
+        else if (controller.b_char3.isPressed()) setSelection(Double.parseDouble(controller.b_char3.getText()));
+        else if (controller.b_char4.isPressed()) setSelection(Double.parseDouble(controller.b_char4.getText()));
     }
 
+    private void setSelection(double value) {
+        if (valueSelected[0] == 0) valueSelected[0] = value;
+        else if (valueSelected[1] == 0) valueSelected[1] = value;
+    }
 
-    boolean sentinel = true;
     private void moveEnime() {
-        double inimiY = controller.iv_enemy1.getY();
+        double enemyY = controller.iv_enemy1.getY();
 
-        if (sentinel) {
+        if (sentinel && enemyY >= 50) sentinel = false;
+        else if (!sentinel && enemyY <= -200) sentinel = true;
 
-            if (inimiY >= 50) {
-                sentinel = false;
-            }
-            if (inimiY <= 50 || inimiY >= -200) {
-                controller.iv_enemy1.setY(inimiY + 8);
-            }
-
-        }
-        if(sentinel==false) {
-
-            if (inimiY <= -200) {
-                sentinel = true;
-            }
-
-            if (inimiY <= 50 || inimiY >= -200) {
-                controller.iv_enemy1.setY(inimiY - 8);
-            }
-
-        }
+        controller.iv_enemy1.setY(enemyY + (sentinel ? 8 : -8));
     }
 
-
-    public void checkAnswer(){
-        if(valueSelected[0] != 0 && valueSelected[1] !=0 && buttonPressed != 0) {
+    public void checkAnswer() {
+        if (valueSelected[0] != 0 && valueSelected[1] != 0 && buttonPressed != 0) {
             switch (buttonPressed) {
-                case 1:
-                       respostPerson =  valueSelected[0] + valueSelected[1];
-                    break;
-                case 2:
-                    respostPerson =  valueSelected[0] - valueSelected[1];
-                    break;
-                case 3:
-                    respostPerson =  valueSelected[0] * valueSelected[1];
-                    break;
-                case 4:
-                    respostPerson =  valueSelected[0] / valueSelected[1];
-                    break;
+                case 1 -> respostPerson = valueSelected[0] + valueSelected[1];
+                case 2 -> respostPerson = valueSelected[0] - valueSelected[1];
+                case 3 -> respostPerson = valueSelected[0] * valueSelected[1];
+                case 4 -> respostPerson = valueSelected[0] / valueSelected[1];
             }
 
-            if(respostPerson != valueEnime){
+            if (respostPerson != valueEnime) {
                 LifeGame.lifeGame--;
-                respostPerson = 0;
-                valueSelected[0] = 0;
-                valueSelected[1] = 0;
-                buttonPressed = 0;
+                resetGameState();
             }
         }
     }
 
+    private void resetGameState() {
+        respostPerson = 0;
+        valueSelected = new double[]{0, 0};
+        buttonPressed = 0;
+    }
 }
